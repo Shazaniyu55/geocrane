8d9d79fb7cbb87d484877688139c77475971575f
shazaniyu55 <shazaniyu@gmail.com>
2026-04-02 18:03:43 +0100

    'server'

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 const connectDB = async () => {
-    const MONGODB_URI = process.env.geocrane_MONGO_URI;
+    const MONGODB_URI = process.env.geocrane_MONGODB_URI;
   
     try {
       if (!MONGODB_URI) return;
